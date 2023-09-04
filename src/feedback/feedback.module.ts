db0cd9870a0bdebf4e2ac4d349df104e24a3669d
Rahul <caulirahul6@gmail.com>
2023-09-04 17:09:56 +0530

    Feedback property setup

--- src/feedback/feedback.module.ts
+++ src/feedback/feedback.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { FeedbackController } from './feedback.controller';
+import { FeedbackService } from './feedback.service';
 
 @Module({
-  controllers: [FeedbackController]
+  controllers: [FeedbackController],
+  providers: [FeedbackService]
 })
 export class FeedbackModule {}
