253d6f083110cbbf936b00c3361726063b6aef4e
Rahul <caulirahul6@gmail.com>
2023-12-31 14:09:46 +0530

    feat: Minio Services

---
+++ src/minio/minio.service.ts
@@ -0,0 +1,52 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import * as Minio from 'minio';
+@Injectable()
+export class MinioService {
+  private minioClient: Minio.Client;
+  private endPoint: string;
+  private port: number;
+  private accessKey: string;
+  private secretKey: string;
+  constructor(private readonly configService: ConfigService) {
+    this.accessKey = configService.getOrThrow('ACCESS_KEY');
+    this.accessKey = configService.getOrThrow('SECRET_KEY');
+    this.accessKey = configService.getOrThrow('PORT');
+    this.accessKey = configService.getOrThrow('END_POINT');
+    this.minioClient = new Minio.Client({
+      endPoint: this.endPoint,
+      port: this.port,
+      useSSL: false,
+      accessKey: this.accessKey,
+      secretKey: this.secretKey,
+    });
+  }
+  async UploadFile(file: Express.Multer.File, key: string) {
+    try {
+      this.minioClient.putObject(
+        this.configService.getOrThrow('BUCKET_NAME'),
+        key,
+        file.buffer,
+        (err) => {
+          console.log(err);
+        },
+      );
+    } catch (e) {
+      return e.message;
+    }
+  }
+
+  async GetfileUrl(key: string) {
+    return `https://my-minio-server.com/${this.configservice.get(
+      'BUCKET_NAME',
+    )}/${key}`;
+  }
+  async DeleteFile(key: string) {
+    try {
+      await this.minioClient.removeObject(
+        this.configService.getOrThrow('BUCKET_NAME'),
+        key,
+      );
+    } catch (e) {}
+  }
+}
