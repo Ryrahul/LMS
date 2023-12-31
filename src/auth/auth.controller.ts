b1b54c671df7d8cda1ba2e55efb7ff945860a09a
Rahul <caulirahul6@gmail.com>
2023-12-31 12:06:39 +0530

    fix: Router

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -11,14 +11,17 @@ import { AuthServices } from './auth.services';
 import { SingupDto } from './dto/Signup.dto';
 import { LoginDto } from './dto/Login.dto';
 import { JwtAuthGuard } from './auth.guard';
+import { Public } from 'src/common/public.decorator';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authservice: AuthServices) {}
+  @Public()
   @Post('signup')
   signup(@Body() dto: SingupDto) {
     return this.authservice.signUp(dto);
   }
+  @Public()
   @Get('verify/:id')
   verification(
     @Param('token') token: string,
@@ -28,6 +31,7 @@ export class AuthController {
 
     return this.authservice.verification( id);
   }
+  @Public()
   @Post('login')
   login(@Body() dto: LoginDto) {
     return this.authservice.login(dto);
