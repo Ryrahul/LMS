0575b9793bc4b6d0a05d54a718743ae6d6d9b4b6
Rahul <caulirahul6@gmail.com>
2024-05-14 20:05:56 +0530

    feat: swagger config

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  const config = new DocumentBuilder()
+    .setTitle('LMS')
+    .setDescription('LMS API description')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3001);
+  app.useGlobalPipes(new ValidationPipe());
+
   console.log(`running on 3001`);
 }
 bootstrap();
